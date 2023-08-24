ed2a3750e35f4144ebef1dcd0764d0e39e0428e0
PrebenS <prebens.kodehode@gmail.com>
2023-08-24 23:17:04 +0200

    search fixes

--- javascript/app.js
+++ javascript/app.js
@@ -7,60 +7,70 @@ import { renderPokemonList } from "./pages/pokemonList.js";
 const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
 const pokemonListURL = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151";
 let pokemonData = [];
-async function catchPokemon() {
+
+async function fetchPokemonDetails(url) {
+  try {
+    const details = await getData(url);
+    return details;
+  } catch (error) {
+    console.error("Error fetching details:", error);
+    return null;
+  }
+}
+
+async function fetchAndRenderPokemonList() {
   try {
     const data = await getData(pokemonListURL);
+    pokemonData = data.results;
 
     cardWrapper.innerHTML = "";
 
-    const pokemonList = data.results;
-    pokemonData = data.results;
-    // creates an array of promises for fetching and rendering to avoid rendering pokemon in the wrong order
-    const renderingPromises = pokemonList.map(async (pokemon) => {
-      try {
-        const details = await getData(pokemon.url);
-        return { pokemon, details };
-      } catch (error) {
-        console.error("Error fetching details for:", pokemon.name, error);
-        return null;
-      }
-    });
-
-    // fetch and render in order
-    for (const renderingPromise of renderingPromises) {
-      const renderingData = await renderingPromise;
-      if (renderingData) {
-        renderPokemonList(renderingData.pokemon, renderingData.details);
-        console.log(renderingData.details);
+    for (const pokemon of pokemonData) {
+      const details = await fetchPokemonDetails(pokemon.url);
+      if (details) {
+        renderPokemonList(pokemon, details);
+        console.log(details);
       }
     }
   } catch (error) {
-    console.error("Error navigating:", error);
+    console.error("Error fetching Pokemon list:", error);
+  }
+}
+
+let searchTimeout;
+
+async function handleSearch() {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  clearTimeout(searchTimeout);
+
+  if (searchTerm.length > 0) {
+    searchTimeout = setTimeout(async () => {
+      const filteredPokemon = pokemonData.filter((pokemon) =>
+        pokemon.name.includes(searchTerm)
+      );
+
+      cardWrapper.innerHTML = "";
+
+      if (filteredPokemon.length > 0) {
+        for (const pokemon of filteredPokemon) {
+          const details = await fetchPokemonDetails(pokemon.url);
+          if (details) {
+            renderPokemonList(pokemon, details);
+          }
+        }
+      } else {
+        cardWrapper.innerHTML = "<h2>No matches found...</h2>";
+      }
+    }, 300);
+  } else {
+    fetchAndRenderPokemonList();
   }
 }
 
 searchInput.addEventListener("input", handleSearch);
 
-function handleSearch() {
-  if (searchInput.value.length > 0) {
-    const filteredPokemon = pokemonData.filter((pokemon) =>
-      pokemon.name.includes(searchInput.value.toLowerCase())
-    );
+fetchAndRenderPokemonList();
 
-    cardWrapper.innerHTML = "";
-
-    filteredPokemon.forEach(async (pokemon) => {
-      try {
-        const details = await getData(pokemon.url);
-        renderPokemonList(pokemon, details);
-      } catch (error) {
-        console.error("Error fetching details for:", pokemon.name, error);
-      }
-    });
-  } else {
-    catchPokemon();
-  }
-}
-catchPokemon();
 // catchPokemon("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151");
 // catchPokemon("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=649");
